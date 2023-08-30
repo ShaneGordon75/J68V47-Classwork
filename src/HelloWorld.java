dbdc4cebc7ef76f1c173d3e6bbde88621c01059e
30134365 <30134365@nescol.ac.uk>
2023-08-30 11:23:20 +0100

    1

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * My First Program
+ * @author Shane Gordon
+ * @version1
+ */
+    public class HelloWorld {
+}
+public static void main (String[] args) {
+//Hello World
+    System.out.println("HelloWorld");
+}
+
+}
